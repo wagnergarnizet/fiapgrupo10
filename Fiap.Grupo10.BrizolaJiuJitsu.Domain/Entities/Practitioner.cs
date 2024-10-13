9a90d17ad28dd5503898cac190e9448617e86045
Guilherme Coelho Garnizet <83613881+guicoelhoo@users.noreply.github.com>
2024-10-13 19:19:26 -0300

    Data Annotation

--- Fiap.Grupo10.BrizolaJiuJitsu.Domain/Entities/Practitioner.cs
+++ Fiap.Grupo10.BrizolaJiuJitsu.Domain/Entities/Practitioner.cs
@@ -1,4 +1,6 @@
 ﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
 
 namespace Fiap.Grupo10.BrizolaJiuJitsu.Domain.Entities
 {
@@ -7,8 +9,11 @@ namespace Fiap.Grupo10.BrizolaJiuJitsu.Domain.Entities
     {
         public Guid Id { get; set; }
 
+        [Required]
+        [DeniedValues("admin", "administrator", "root","",null,"string", ErrorMessage = "Nome de usuário não permitido!")]
         public required string FullName { get; set; }
 
+        [AllowedValues("A+", "A-", "B+", "B-", "AB+", "AB-", "O+", "O-", ErrorMessage = "Valor não permitido!")]
         public string? BloodType { get; set; }
     }
 }
